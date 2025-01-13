f3dda23bbeeb3ef2d1226f03d1339db2429cbd24
akshukla2424 <146557837+akshukla2424@users.noreply.github.com>
2025-01-13 19:20:00 +0000

    conversionOperation

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+console.log("HAR HAR MAHADEV");
+
+let score = "33"
+let st = "kuwar"
+
+console.log(typeof 
+score);
+console.log(typeof 
+(st));
+ 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1, false => 0
+*/
+
+let isLoggedIn = "Ashutosh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Ashutosh" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
