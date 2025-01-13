9844f44be3ecc904d31a2d8b80616f17b82566ae
akshukla2424 <146557837+akshukla2424@users.noreply.github.com>
2025-01-13 19:57:02 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -32,3 +32,52 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+console.log("/////////////operations////////////")
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)
+*/
+
+let str1 = "radheShyam"
+let str2 = " sitaRam"
+console.log(str1 + str2)
+
+console.log("1" + 2); //12
+console.log("1" + "2"); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 3); //123
+console.log(1 + 2 + "2"); //32
+
+console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2*2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
