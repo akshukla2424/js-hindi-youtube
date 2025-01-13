7899247bd4b4b084ec2ff4450ec50d32947321c3
akshukla2424 <146557837+akshukla2424@users.noreply.github.com>
2025-01-13 18:52:49 +0000

    dataTypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+console.log("JAY JAY SHRI RAM")
+let name = "Ashutosh"
+let age = 22
+let isLoggedIn = false
+
+/*
+number => 2 to ower 53
+bigint => 
+string => ""
+boolean => true/False
+null => standalone value not 0
+undefined => not assigned value
+symbol => unique
+object
+*/
+
+console.log(typeof null) //object
+
+console.log(typeof undefined)// undefined
