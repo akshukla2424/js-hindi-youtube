98824268acaa10ee9770c532d1ce98b24ccaa592
akshukla2424 <146557837+akshukla2424@users.noreply.github.com>
2025-01-14 18:52:13 +0000

    string

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -8,7 +8,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 const gameName = new String("Cricket")
 
 console.log(gameName[0]);
-console.log(gameName.toUpperCase);
+console.log(gameName.toUpperCase());
 console.log(gameName.length);
 
 
