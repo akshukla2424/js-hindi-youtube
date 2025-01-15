9c2485f675f0b54067c66f490a08340cf0072fff
akshukla2424 <146557837+akshukla2424@users.noreply.github.com>
2025-01-15 09:03:13 +0000

    string

--- .devcontainer/01_basics/05_string.js
+++ .devcontainer/01_basics/05_string.js
@@ -1,15 +1,40 @@
 const name = "Ashutosh"
-const repo = 5
+const repoCount = 5
 
-console.log(name + repoCount)
+console.log(name + " " + repoCount)
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String("Cricket")
+const gameName = new String("Cricket-badminton-vollyball")
 
 console.log(gameName[0]);
 console.log(gameName.toUpperCase());
 console.log(gameName.length);
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString1 = "    Ashutosh   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://didactic-trout-979r44qvq5qw2x699.github.dev/"
+
+console.log(url.replace('dev','in'));
+//console.log(url.includes(sunder));
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
 
 
 
